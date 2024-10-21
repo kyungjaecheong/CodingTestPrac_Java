d32a791730c19c4e115a1ccbdc660f4f7cd08181
KyungJae Cheong <dankj1991@gmail.com>
2024-10-21 20:57:13 +0900

    ZB_BE_Month1_Week4 - Pre 코테 (힌트 문제) (24.10.21)

--- 2024/10/Week4/00_Hint/questions/Main.java
+++ 2024/10/Week4/00_Hint/questions/Main.java
@@ -19,11 +19,48 @@ public class Main {
         String s = Integer.toBinaryString(sum);
         s = s.replace("2", "0");
         result+= Integer.parseInt(s,2);
-         */
         int xorValue = Integer.parseInt(biNums[i],2) ^ Integer.parseInt(biNums[j], 2);
         result += xorValue;
+        result += xorByBits(biNums[i], biNums[j]);
+         */
+        String a = biNums[i];
+        String b = biNums[j];
+        int maxLength = Math.max(a.length(), b.length());
+        a = String.format("%" + maxLength + "s", a).replace(' ', '0');
+        b = String.format("%" + maxLength + "s", b).replace(' ', '0');
+
+        StringBuilder xorResult = new StringBuilder();
+        for (int k = 0; k < maxLength; k++) {
+          char bitA = a.charAt(k);
+          char bitB = b.charAt(k);
+
+          // XOR 연산
+          xorResult.append((bitA == bitB) ? '0' : '1');
+        }
+        result += Integer.parseInt(xorResult.toString(), 2);
       }
     }
     System.out.println(result);
   }
+  // 두 이진수 문자열을 자리수별로 XOR 연산하는 함수
+//  public static int xorByBits(String a, String b) {
+//    // 두 문자열의 길이를 맞추기 위해 짧은 쪽을 0으로 채운다
+//    int maxLength = Math.max(a.length(), b.length());
+//    a = String.format("%" + maxLength + "s", a).replace(' ', '0');
+//    b = String.format("%" + maxLength + "s", b).replace(' ', '0');
+//
+//    StringBuilder xorResult = new StringBuilder();
+//
+//    // 각 비트를 순회하면서 XOR 연산
+//    for (int i = 0; i < maxLength; i++) {
+//      char bitA = a.charAt(i);
+//      char bitB = b.charAt(i);
+//
+//      // XOR 연산
+//      xorResult.append((bitA == bitB) ? '0' : '1');
+//    }
+//
+//    // XOR 결과를 2진수에서 10진수로 변환 후 반환
+//    return Integer.parseInt(xorResult.toString(), 2);
+//  }
 }
